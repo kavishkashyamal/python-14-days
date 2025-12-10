715a8c46bcfd0044b10602395cbd5db56cc9b422
Kavishka Shyamal <shyamalkavishka@gmail.com>
2025-12-10 20:35:44 +0530

    Fix spelling of Celsius in temperature converter
    Corrected all instances of 'Celcius' to 'Celsius' in function names, variable names, and user prompts for clarity and accuracy in the temperature conversion logic.

--- day-01/convertor.py
+++ day-01/convertor.py
@@ -4,28 +4,28 @@ print("Choose your number to continue : ")
 choice=input(" 1. Temperature \n 2. Distance \n Enter your value : ")
 
 def temperature():
-    temperature_choice = input(" 1. Celcius to Kelvin \n 2. Kelvin to Celcius ")
+    temperature_choice = input(" 1. Celsius to Kelvin \n 2. Kelvin to Celsius ")
 
-    def celcius_to_kelvin():
-            print("Celcius to Kelvin...")
-            celcius_input=float(input("Enter your Value : "))
-            kelvin_output=celcius_input+273.15
+    def Celsius_to_kelvin():
+            print("Celsius to Kelvin...")
+            Celsius_input=float(input("Enter your Value : "))
+            kelvin_output=Celsius_input+273.15
 
-            print(f"Your Input value is {celcius_input}°C. A change of 1°C is exactly equal to a change of 1 K.. \n T(k)=T(c)+273.15 , k = Kelvin and c = Celcious \n Your answer is {kelvin_output}K")
+            print(f"Your Input value is {Celsius_input}°C. A change of 1°C is exactly equal to a change of 1 K.. \n T(k)=T(c)+273.15 , k = Kelvin and c = Celcious \n Your answer is {kelvin_output}K")
             
             
-    def kelvin_to_celcius():
-            print("Kelvin to Celcius...")
+    def kelvin_to_Celsius():
+            print("Kelvin to Celsius...")
             kelvin_input=float(input("Enter your value : "))
-            celcius_output=kelvin_input-273.15
-            print(celcius_output)
+            Celsius_output=kelvin_input-273.15
+            print(Celsius_output)
 
     while True:
         try:
             if temperature_choice == "1":
-                celcius_to_kelvin()
+                Celsius_to_kelvin()
             elif temperature_choice == "2":
-                kelvin_to_celcius()
+                kelvin_to_Celsius()
             else:
                 print("Invalid choice!")
             break
